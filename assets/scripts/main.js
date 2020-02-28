001c4182c2ffd7a6c36a6d6d088bca304a44730a
melanie Boardman <melanie_boardman@hotmail.com>
2020-02-28 21:32:40 +0000

    High Last button functionality added

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,15 +2,27 @@
 let on = false;
 // Strict is always OFF when page is initially loaded.
 let strict = false;
+// The computer always plays first 
+let compTurn = true;
+// The score always starts as 0
+let score = 0;
+// The last score is always displayed when the page is initally loaded (More work needed here?)
+let highLast = "last";
+let highScore = 0;
+let lastScore = 0;
 
 // scoreDisplay is the central display in the display window
 const scoreDisplay = document.querySelector("#score-display");
 // strictDisplay is top left in the display window
 const strictDisplay = document.querySelector("#strict-display")
+// High / Last is displayed in the top right of the display window
+const highLastDisplay = document.querySelector("#high-last-display")
 // State of the On/Off button
 const onOffButton = document.querySelector("#on-off");
 // State of the Strict button
 const strictButton = document.querySelector("#strict");
+// State of the HighLast button
+const highLastButton = document.querySelector("#high-last");
 
 
 
@@ -29,11 +41,27 @@ onOffButton.addEventListener('click', (event) => {
 
 // Strict ON or OFF - sets variable and changes text in display window
 strictButton.addEventListener('click', (event) => {
-    if (strict === false) {
-        strict = true;
-        strictDisplay.innerHTML = "STRICT: ON";
-    } else {
-        strict = false;
-        strictDisplay.innerHTML = "STRICT: OFF";
+    if (on === true) {
+        if (strict === false) {
+            strict = true;
+            strictDisplay.innerHTML = "STRICT: ON";
+        } else {
+            strict = false;
+            strictDisplay.innerHTML = "STRICT: OFF";
+        }
+    }
+
+});
+
+//High / last button  - toggles between the High Score and the last Score on the Display
+highLastButton.addEventListener('click', (event) => {
+    if (on === true) {
+        if (highLast === "last") {
+            highLast = "high";
+            highLastDisplay.innerHTML = "HIGH: " + highScore;
+        } else {
+            highLast = "last";
+            highLastDisplay.innerHTML = "LAST: " + lastScore;
+        }
     }
 });
