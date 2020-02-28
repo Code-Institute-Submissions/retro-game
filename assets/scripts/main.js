1521e07fe441c99c422f268b68a854678912b388
melanie Boardman <melanie_boardman@hotmail.com>
2020-02-28 22:09:16 +0000

    Created function playGame() and added random sequence generator

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,7 +4,7 @@ let on = false;
 let strict = false;
 // The computer always plays first 
 let compTurn = true;
-// The score always starts as 0
+// The score always starts as 0 (maybe add compSequence.length)
 let score = 0;
 // The last score is always displayed when the page is initally loaded (More work needed here?)
 let highLast = "last";
@@ -13,6 +13,13 @@ let lastScore = 0;
 
 // The Game is not started when the page loads
 let start = false;
+
+//Array to hold random sequence
+let sequence = [];
+//Array to hold computer sequence (1 added from random sequence each turn)
+let compSequence = [];
+//Array to hold player sequence ()
+let playerSequence = [];
 
 // scoreDisplay is the central display in the display window
 const scoreDisplay = document.querySelector("#score-display");
@@ -46,7 +53,7 @@ onOffButton.addEventListener('click', (event) => {
 
 // Strict ON or OFF - sets variable and changes text in display window
 strictButton.addEventListener('click', (event) => {
-    if (on === true) {
+    if (on === true && start === false) {
         if (strict === false) {
             strict = true;
             strictDisplay.innerHTML = "STRICT: ON";
@@ -87,3 +94,16 @@ startResetButton.addEventListener('click', (event) => {
         }
     }});
     
+function playGame(){
+    score = 0;
+    playerSequence = [];
+    compSequence = [];
+    // Populate random sequence with numbers between 1 and 4.
+    // loop currently set to 8 but can use skill level when this has been implemented
+    for (i=0; i<8; i++){
+        //math.random returns number between 0 and 1. 
+        //I have multiplied this by 4 and then use math.floor to round it down to the nearest whole number.
+        //I have then added 1. This ensures that the number is always between 1 and 4
+        sequence.push(Math.floor(Math.random() * 4) + 1);}
+    //Need to determine turns and add items from the random sequence to the compSeq.......TBC...
+}
