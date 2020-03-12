f88a72dc1e354988113aaa75440c5957aec95283
melanie Boardman <melanie_boardman@hotmail.com>
2020-03-12 20:54:19 +0000

    updated Unit Tests

--- unit-tests/jasmine-standalone-3.5.0/spec/generateSeqSpec.js
+++ unit-tests/jasmine-standalone-3.5.0/spec/generateSeqSpec.js
@@ -1,9 +1,16 @@
 
 
-describe("generate sequence", function() { 
+describe("generateSequence", function() { 
+
+    var seqLength
+
+    it("should Exist", function(){
+        expect(generateSequence()).toBeDefined();
+    });
    
-    it("should exist",function() { 
-       expect(generateSequence()).toBeDefined; 
+    it("should return a sequence",function() { 
+       expect(generateSequence(seqLength)).toEqual; 
     }); 
  
  });
+
--- unit-tests/jasmine-standalone-3.5.0/src/generateSeq.js
+++ unit-tests/jasmine-standalone-3.5.0/src/generateSeq.js
@@ -1,7 +1,16 @@
-function Song() {
-}
+//==============GENERATE SEQUENCE=========================//
+function generateSequence(seqLength){
+  let sequence = [];
+  // Populate random sequence with numbers between 1 and 4.
+  // loop currently set to 8 but can use skill level when this has been implemented
+  for (i = 0; i < seqLength; i++) {
+      //math.random returns number between 0 and 1. 
+      //I have multiplied this by 4 and then use math.floor to round it down to the nearest whole number.
+      //I have then added 1. This ensures that the number is always between 1 and 4
+      sequence.push(Math.floor(Math.random() * 4) + 1);
+  }
+  console.log("DEBUG: generate Sequence function: sequence = " + sequence); //for debug
 
-Song.prototype.persistFavoriteStatus = function(value) {
-  // something complicated
-  throw new Error("not yet implemented");
-};
+  return sequence;
+
+}
